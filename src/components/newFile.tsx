7cd401cd0af21b051f677f2faca382cc9ad64fdc
William Rodríguez Cruz <willycruzo@outlook.com>
2024-01-13 09:56:54 -0500

    Creación portafolio William Cruz con Astro

---
+++ src/components/newFile.tsx
@@ -0,0 +1,14 @@
+<Fragment>
+<header class="flex justify-center items-center 
+mx-auto py-5 w-full xl:w-[1120px]">
+
+<nav class="flex flex-row items-center gap-x-10 opacity-80">
+<a href="#top"> Home</a>
+<a href="#about">About</a>
+<a href="#experience">Experience</a>
+<a href="#projects">Projects</a>
+<a href="#contact">Contact</a>
+</nav>
+
+</header>
+</Fragment>;
